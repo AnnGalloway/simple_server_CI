d7f1d6950c708fd3dab613745f349a3622dc3a6d
AnnGalloway <60460582+AnnGalloway@users.noreply.github.com>
2023-12-13 16:06:36 +0000

    tests fixed

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ def test_get_home(web_client):
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Hello, world!"
 
-def test_get_home(web_client):
+def test_get_home2(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world"
+    assert response.data.decode("utf-8") == "Hello, world!"
